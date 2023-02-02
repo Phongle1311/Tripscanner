bd9abec063177961f916369ee6d089ec40103828
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2023-02-02 13:50:55 +0700

    config tailwind-react-native

--- App.tsx
+++ App.tsx
@@ -1,3 +1,12 @@
+import { View, Text } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+
 export default function App() {
-  return <></>;
+  return (
+    <TailwindProvider>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </TailwindProvider>
+  );
 }
